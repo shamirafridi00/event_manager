37e44ab2f7d8da7a9dacc8f9941c025f70db5e81
Shamir Afridi <shamirafridi00@gmail.com>
2023-09-29 20:37:59 +0500

    event_manager

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
 civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-template_letter = File.read('form_letter.html')
+
 
 
 
@@ -49,7 +50,7 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
+    ).officials
     legislators = legislators.officials
     legislator_names = legislators.map(&:name)
     legislator_names.join(", ")
@@ -59,13 +60,29 @@ def legislators_by_zipcode(zip)
 end
 
 
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+
+template_letter = File.read('../form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} lives in #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
 end
